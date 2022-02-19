926b9fd28bcbad897aaa7dd6fb6bb27d2c5d3129
wenlongmac <2854588836@qq.com>
2022-02-19 16:30:47 +0800

    feat:基本页面效果，有接口数据

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <h1>1测试页面</h1>
+  <ul>
+  <li v-for="(item) in list" :key="item.id">{{item.name}}</li>
+  </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data(){
+    return {
+      list: [
+        { id: 5, name: '测试猿'}
+      ],
+    }
+  },
+  mounted(){
+    axios.get('/api/users').then(res => {
+      console.log(res)
+      this.list = res.data || [];
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
